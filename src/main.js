17e0e50df466c5fc9f6ad69124440998c97c48ec
goddlts <54337467@qq.com>
2018-08-07 11:00:05 +0800

    路由对象

--- src/main.js
+++ src/main.js
@@ -11,7 +11,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 // 导入index.css
 import './assets/css/index.css';
 
+// 导入路由模块
+import router from './routes/router';
+
 new Vue({
+  router,
   el: '#app',
   render: h => h(App)
-})
+});
---
+++ src/routes/router.js
@@ -0,0 +1,16 @@
+import VueRouter from 'vue-router';
+import Vue from 'vue';
+
+// 注册VueRouter插件  this.$router  this.$route
+Vue.use(VueRouter);
+
+// 创建路由对象
+var router = new VueRouter({
+  // 路由规则
+  routes: [
+
+  ]
+});
+
+// 模块中导出路由对象
+export default router;
