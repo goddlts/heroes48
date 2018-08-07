6ed49e1494be60b171bfe10bfdc5d548cb0b6ded
goddlts <54337467@qq.com>
2018-08-07 11:28:18 +0800

    配置路由规则

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,5 +1,11 @@
 import VueRouter from 'vue-router';
 import Vue from 'vue';
+// 导入组件
+import HeroesList from '../views/heroes/List.vue';
+import EquipsList from '../views/equips/List.vue';
+import WeaponsList from '../views/weapons/List.vue';
+
+
 
 // 注册VueRouter插件  this.$router  this.$route
 Vue.use(VueRouter);
@@ -8,7 +14,9 @@ Vue.use(VueRouter);
 var router = new VueRouter({
   // 路由规则
   routes: [
-
+    { name: 'heroes', path: '/heroes', component: HeroesList },
+    { name: 'equips', path: '/equips', component: EquipsList },
+    { name: 'weapons', path: '/weapons', component: WeaponsList }
   ]
 });
 
