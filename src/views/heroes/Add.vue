41af189524e90df2c9447dd487238a70bb6fae78
goddlts <54337467@qq.com>
2018-08-07 15:53:36 +0800

    添加英雄

--- src/views/heroes/Add.vue
+++ src/views/heroes/Add.vue
@@ -4,20 +4,51 @@
     <form>
       <div class="form-group">
         <label for="name">英雄名称</label>
-        <input type="text" class="form-control" id="name" placeholder="英雄名称">
+        <input v-model="formData.name" type="text" class="form-control" id="name" placeholder="英雄名称">
       </div>
       <div class="form-group">
         <label for="sex">英雄性别</label>
-        <input type="text" class="form-control" id="sex" placeholder="英雄性别">
+        <input v-model="formData.gender" type="text" class="form-control" id="sex" placeholder="英雄性别">
       </div>
-      <button type="submit" class="btn btn-success">Submit</button>
+      <button @click.prevent="handleAdd" type="submit" class="btn btn-success">Submit</button>
     </form>
   </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+  data() {
+    return {
+      // 1 绑定文本框的数据，将来也是post给服务器的数据
+      formData: {
+        name: '',
+        gender: ''
+      }
+    };
+  },
+  methods: {
+    // 2 添加英雄
+    handleAdd() {
+      // 发送ajax请求
+      axios
+        .post('http://127.0.0.1:3001/heroes', this.formData)
+        .then((response) => {
+          // 判断是否添加成功
+          if (response.status === 201) {
+            // 跳转到列表页
+            this.$router.push('/heroes');
+          } else {
+            // 失败
+            alert('添加失败');
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+    }
+  }
 };
 </script>
 
