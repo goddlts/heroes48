cfef23c4baf9a8efe300233178ed135eb6ca5ea4
goddlts <54337467@qq.com>
2018-08-07 11:39:29 +0800

    配置导航菜单

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="col-sm-3 col-md-2 sidebar">
     <ul class="nav nav-sidebar">
-      <li class="active"><a href="#">英雄列表</a></li>
+      <!-- <li class="active"><a href="#">英雄列表</a></li>
       <li><a href="#">武器列表</a></li>
-      <li><a href="#">装备列表</a></li>
+      <li><a href="#">装备列表</a></li> -->
+      <router-link tag="li" to="/heroes">
+        <a>英雄列表</a>
+      </router-link>
+      <router-link tag="li" to="/equips">
+        <a>装备列表</a>
+      </router-link>
+      <router-link tag="li" to="/weapons">
+        <a>武器列表</a>
+      </router-link>
     </ul>
   </div>
 </template>
--- src/routes/router.js
+++ src/routes/router.js
@@ -12,8 +12,11 @@ Vue.use(VueRouter);
 
 // 创建路由对象
 var router = new VueRouter({
+  // 配置激活的类样式
+  linkExactActiveClass: 'active',
   // 路由规则
   routes: [
+    { path: '/', redirect: '/heroes' },
     { name: 'heroes', path: '/heroes', component: HeroesList },
     { name: 'equips', path: '/equips', component: EquipsList },
     { name: 'weapons', path: '/weapons', component: WeaponsList }
